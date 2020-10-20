086f4659043f335a17a5edceaa3b9300b9d3016f
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-20 11:05:56 +0200

    Legg til craco og litt config

---
+++ craco.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const buildPath = path.resolve(__dirname, './build');
+const CracoLessPlugin = require('craco-less');
+const cssprefixer = require('postcss-prefix-selector');
+
+const RemoveCssHashPlugin = {
+    overrideWebpackConfig: ({ webpackConfig }) => {
+        const plugins = webpackConfig.plugins;
+        plugins.forEach((plugin) => {
+            const options = plugin.options;
+
+            if (!options) {
+                return;
+            }
+
+            if (options.filename && options.filename.endsWith('.css')) {
+                console.log('rewriting target', options);
+                options.filename = 'css/[name].css';
+                options.chunkFilename = 'css/[name].chunk.css';
+            }
+        });
+
+        return webpackConfig;
+    },
+};
+
+const RemoveJsHashPlugin = {
+    overrideCracoConfig: ({ cracoConfig }) => {
+        cracoConfig.webpack = {
+            configure: {
+                optimization: {
+                    splitChunks: {
+                        cacheGroups: {
+                            default: false,
+                            vendors: false,
+                        },
+                    },
+                    runtimeChunk: false,
+                },
+                output: {
+                    path: buildPath,
+                    filename: 'js/rekrutteringsbistand-header.js',
+                },
+            },
+        };
+
+        return cracoConfig;
+    },
+};
+
+module.exports = {
+    style: {
+        postcss: {
+            plugins: [
+                cssprefixer({
+                    prefix: '.header',
+                    exclude: ['html', 'body', '.header'],
+                    transform: function (prefix, selector, prefixedSelector) {
+                        if (selector.startsWith('body ')) {
+                            return `body ${prefix} ${selector.slice(5)}`;
+                        } else if (selector.startsWith('html ')) {
+                            return `html ${prefix} ${selector.slice(5)}`;
+                        } else if (selector.startsWith('.dekorator ')) {
+                            return selector;
+                        } else if (selector.includes('modal')) {
+                            return selector;
+                        }
+                        return prefixedSelector;
+                    },
+                }),
+            ],
+        },
+    },
+    plugins: [
+        { plugin: CracoLessPlugin },
+        { plugin: RemoveCssHashPlugin },
+        { plugin: RemoveJsHashPlugin },
+    ],
+};
