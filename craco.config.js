d95d2cac868ccdfccfb10e5ffc75c9f07dda50cb
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-26 15:42:51 +0100

    Ikke eksporter med navspa, sett rett på 'window'

--- craco.config.js
+++ craco.config.js
@@ -1,18 +1,51 @@
+const path = require('path');
+const buildPath = path.resolve(__dirname, './build');
 const CracoLessPlugin = require('craco-less');
 const cssprefixer = require('postcss-prefix-selector');
 
-const transformerCss = (prefix, selector, prefixedSelector) => {
-    if (selector.startsWith('body ')) {
-        return `body ${prefix} ${selector.slice(5)}`;
-    } else if (selector.startsWith('html ')) {
-        return `html ${prefix} ${selector.slice(5)}`;
-    } else if (selector.startsWith('.statistikk ')) {
-        return selector;
-    } else if (selector.includes('modal')) {
-        return selector;
-    }
+const RemoveCssHashPlugin = {
+    overrideWebpackConfig: ({ webpackConfig }) => {
+        const plugins = webpackConfig.plugins;
+        plugins.forEach((plugin) => {
+            const options = plugin.options;
 
-    return prefixedSelector;
+            if (!options) {
+                return;
+            }
+
+            if (options.filename && options.filename.endsWith('.css')) {
+                console.log('rewriting target', options);
+                options.filename = 'static/css/[name].css';
+                options.chunkFilename = 'static/css/[name].chunk.css';
+            }
+        });
+
+        return webpackConfig;
+    },
+};
+
+const RemoveJsHashPlugin = {
+    overrideCracoConfig: ({ cracoConfig }) => {
+        cracoConfig.webpack = {
+            configure: {
+                optimization: {
+                    splitChunks: {
+                        cacheGroups: {
+                            default: false,
+                            vendors: false,
+                        },
+                    },
+                    runtimeChunk: false,
+                },
+                output: {
+                    path: buildPath,
+                    filename: 'static/js/rekrutteringsbistand-statistikk.js',
+                },
+            },
+        };
+
+        return cracoConfig;
+    },
 };
 
 module.exports = {
@@ -22,10 +55,25 @@ module.exports = {
                 cssprefixer({
                     prefix: '.statistikk',
                     exclude: ['html', 'body', '.statistikk'],
-                    transform: transformerCss,
+                    transform: function (prefix, selector, prefixedSelector) {
+                        if (selector.startsWith('body ')) {
+                            return `body ${prefix} ${selector.slice(5)}`;
+                        } else if (selector.startsWith('html ')) {
+                            return `html ${prefix} ${selector.slice(5)}`;
+                        } else if (selector.startsWith('.statistikk ')) {
+                            return selector;
+                        } else if (selector.includes('modal')) {
+                            return selector;
+                        }
+                        return prefixedSelector;
+                    },
                 }),
             ],
         },
     },
-    plugins: [{ plugin: CracoLessPlugin }],
+    plugins: [
+        { plugin: CracoLessPlugin },
+        { plugin: RemoveCssHashPlugin },
+        { plugin: RemoveJsHashPlugin },
+    ],
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navspa from '@navikt/navspa';
 import App from './App';
 import Utviklingsapp from './utviklingsapp/Utviklingsapp';
 import './index.css';
@@ -9,7 +8,7 @@ const erProd = process.env.NODE_ENV === 'production';
 const eksporterApp = process.env.REACT_APP_EXPORT || erProd;
 
 if (eksporterApp) {
-    Navspa.eksporter('rekrutteringsbistand-statistikk', App);
+    (window as any)['rekrutteringsbistand-statistikk'] = App;
 } else {
     ReactDOM.render(<Utviklingsapp />, document.getElementById('utviklingsapp'));
 }
