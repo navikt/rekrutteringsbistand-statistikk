ece99d26d7b8e324b6d0849d80c4a8b537166b98
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-28 11:59:55 +0100

    Deploy til dev

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import NAVSPA from '@navikt/navspa';
 import App from './App';
 import Utviklingsapp from './utviklingsapp/Utviklingsapp';
-import eksporterApp from './eksporterApp';
 import './index.css';
 
 const erProd = process.env.NODE_ENV === 'production';
@@ -11,7 +10,6 @@ const skalEksporteres = process.env.REACT_APP_EXPORT || erProd;
 
 if (skalEksporteres) {
     NAVSPA.eksporter('rekrutteringsbistand-statistikk', App);
-    eksporterApp('rekrutteringsbistand-statistikk', App);
 } else {
     ReactDOM.render(<Utviklingsapp />, document.getElementById('utviklingsapp'));
 }
