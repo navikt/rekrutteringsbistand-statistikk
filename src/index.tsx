2aca8bd2bcea9b789bd86188664b1f74265604b9
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-10-28 08:35:18 +0100

    Demonstrer feil med navspa

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NAVSPA from '@navikt/navspa';
 import App from './App';
 import Utviklingsapp from './utviklingsapp/Utviklingsapp';
 import eksporterApp from './eksporterApp';
@@ -9,6 +10,7 @@ const erProd = process.env.NODE_ENV === 'production';
 const skalEksporteres = process.env.REACT_APP_EXPORT || erProd;
 
 if (skalEksporteres) {
+    NAVSPA.eksporter('rekrutteringsbistand-statistikk', App);
     eksporterApp('rekrutteringsbistand-statistikk', App);
 } else {
     ReactDOM.render(<Utviklingsapp />, document.getElementById('utviklingsapp'));
