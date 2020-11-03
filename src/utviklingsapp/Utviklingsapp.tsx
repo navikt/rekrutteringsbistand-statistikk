61a1951965635efbf4cd51612bfbc318bae40f09
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-11-03 10:39:45 +0100

    Ikke ha et CSS prefix-unntak for utviklingsapp

--- src/utviklingsapp/Utviklingsapp.tsx
+++ src/utviklingsapp/Utviklingsapp.tsx
@@ -3,10 +3,16 @@ import { Systemtittel } from 'nav-frontend-typografi';
 import App from '../App';
 import './Utviklingsapp.less';
 
+// Alle klassenavn blir prefikset med ".statistikk" i craco-configen, så også koden
+// som brukes under utvikling må wrappes i et element med dette klassenavnet.
+const klassenavnForHeader = 'statistikk';
+
 const Utviklingsapp: FunctionComponent = () => (
     <>
-        <header className="utviklingsapp">
-            <Systemtittel>Utviklingsapp for rekrutteringsbistand-statistikk</Systemtittel>
+        <header className={klassenavnForHeader}>
+            <Systemtittel className="utviklingsapp">
+                Utviklingsapp for rekrutteringsbistand-statistikk
+            </Systemtittel>
         </header>
         <main>
             <App navKontor="0239" />
