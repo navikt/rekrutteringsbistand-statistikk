9c8acb5242c6dbef484296690c5c2031e593ac42
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-11-06 10:55:13 +0100

    Match url med middleware

--- server/server.js
+++ server/server.js
@@ -9,19 +9,18 @@ const statistikkApiUrl = process.env.STATISTIKK_API_URL;
 const basePath = '/statistikk';
 const buildPath = path.join(__dirname, '../build');
 
-const proxyTilStatistikkApi = createProxyMiddleware({
-    target: statistikkApiUrl,
-    changeOrigin: true,
-    pathRewrite: {
-        '^/statistikk/api': '',
-    },
-});
+const proxyTilStatistikkApi = (url) =>
+    createProxyMiddleware(url, {
+        target: statistikkApiUrl,
+        changeOrigin: true,
+        pathRewrite: (path) => path.replace(url, ''),
+    });
 
 const startServer = () => {
     app.use(`${basePath}/static`, express.static(buildPath + '/static'));
     app.use(`${basePath}/asset-manifest.json`, express.static(`${buildPath}/asset-manifest.json`));
 
-    app.use(`${basePath}/api/*`, proxyTilStatistikkApi);
+    app.use(proxyTilStatistikkApi(`${basePath}/api`));
 
     app.get(`${basePath}/internal/isAlive`, (req, res) => res.sendStatus(200));
     app.get(`${basePath}/internal/isReady`, (req, res) => res.sendStatus(200));
