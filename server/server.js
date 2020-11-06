30e9899c889350e5931cbf1bcca7a3c9febf898d
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-11-06 10:09:41 +0100

    Sett opp proxy til Statistikk-API via node

--- server/server.js
+++ server/server.js
@@ -4,17 +4,27 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 const port = process.env.PORT || 3000;
+const statistikkApiUrl = process.env.STATISTIKK_API_URL;
+
 const basePath = '/statistikk';
 const buildPath = path.join(__dirname, '../build');
+
+const proxyTilStatistikkApi = createProxyMiddleware({
+    target: statistikkApiUrl,
+    changeOrigin: true,
+    pathRewrite: {
+        '^/statistikk/api': '',
+    },
+});
 
 const startServer = () => {
     app.use(`${basePath}/static`, express.static(buildPath + '/static'));
     app.use(`${basePath}/asset-manifest.json`, express.static(`${buildPath}/asset-manifest.json`));
 
+    app.use(`${basePath}/api`, proxyTilStatistikkApi);
+
     app.get(`${basePath}/internal/isAlive`, (req, res) => res.sendStatus(200));
     app.get(`${basePath}/internal/isReady`, (req, res) => res.sendStatus(200));
-
-    app.use(`${basePath}/api`, createProxyMiddleware({ target: '', changeOrigin: true }));
 
     app.listen(port, () => {
         console.log('Server kjører på port', port);
