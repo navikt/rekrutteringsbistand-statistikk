607d51faae88350896e4cd06d54818ae87a8a291
André Mossige <mossige.a@gmail.com>
2020-11-13 12:27:03 +0100

    Sjekk om ny URL fungerer

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const startServer = () => {
     app.use(`${basePath}/static`, express.static(buildPath + '/static'));
     app.use(`${basePath}/asset-manifest.json`, express.static(`${buildPath}/asset-manifest.json`));
 
-    app.use(setupProxy(`/statistikk/api`, statistikkApiUrl));
+    app.use(setupProxy(`${basePath}/api`, statistikkApiUrl));
 
     app.get(`${basePath}/internal/isAlive`, (req, res) => res.sendStatus(200));
     app.get(`${basePath}/internal/isReady`, (req, res) => res.sendStatus(200));
