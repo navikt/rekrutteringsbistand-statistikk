a5b8bd22f4709d3dfaeba266dbb6296951a33e60
madsinav <74543378+madsinav@users.noreply.github.com>
2020-11-18 10:45:47 +0100

    Advarsel feil med presentert (#9)
    * Vis advarsel om feil i presentert-tall
    
    * Deploy til dev
    
    * Fjern lint-feil

--- src/statistikk/Statistikk.tsx
+++ src/statistikk/Statistikk.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useEffect, useState } from 'react';
 import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import { idag, trettiDagerSiden, formaterDatoTilApi, formaterDatoTilVisning } from '../datoUtils';
 import Telling from './Telling';
+import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
 import './Statistikk.less';
 
 type Props = {
@@ -65,10 +66,14 @@ const Statistikk: FunctionComponent<Props> = ({ navKontor }) => {
                     className="telling--fått-jobb"
                 />
                 <Telling
-                    tall={antallPresentert}
+                    tall="-"
                     beskrivelse={beskrivelseForAntallPresentert}
                     className="telling--presentert"
                 />
+                <div />
+                <AlertStripeAdvarsel>
+                    Tallene for presentert er feil. Vi jobber med å rette dette.
+                </AlertStripeAdvarsel>
             </div>
         </div>
     );
