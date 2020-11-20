c637fbdeab431a945e22108e09d99d122729605b
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-11-20 10:02:32 +0100

    Bruk lenker fra react-router til stillingsappen

--- src/mock/mock-api.ts
+++ src/mock/mock-api.ts
@@ -2,4 +2,6 @@ import fetchMock from 'fetch-mock';
 import { statistikkApiUrl } from '../statistikk/Statistikk';
 import { antallFormidlinger } from './testdata';
 
+fetchMock.config.fallbackToNetwork = true;
+
 fetchMock.get(`begin:${statistikkApiUrl}`, antallFormidlinger);
