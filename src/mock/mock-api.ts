c90f9c21988464842ec6fcadc489b63b37fa6dac
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2021-01-28 07:54:26 +0100

    Eksporter med navspa (#14)
    * Eksporter app med Navspa
    
    * Deploy til dev
    
    * Bedre mock for NAV-kontor for å verifisere ny navspa-løsning

--- src/mock/mock-api.ts
+++ src/mock/mock-api.ts
@@ -1,7 +1,12 @@
 import fetchMock from 'fetch-mock';
 import { statistikkApiUrl } from '../statistikk/Statistikk';
-import { antallFormidlinger } from './testdata';
+import { hentAntallFormidlinger } from './testdata';
 
 fetchMock.config.fallbackToNetwork = true;
 
-fetchMock.get(`begin:${statistikkApiUrl}`, antallFormidlinger);
+fetchMock.get(`begin:${statistikkApiUrl}`, (url) => {
+    const searchQuery = url.split('?').pop();
+    const navKontor = new URLSearchParams(searchQuery).get('navKontor');
+
+    return hentAntallFormidlinger(navKontor || '0000');
+});
