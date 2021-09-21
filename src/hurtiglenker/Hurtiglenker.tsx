1f6d3471320d8c4c6fc9313a023b2a7a172c9f38
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-09-21 09:46:33 +0200

    Fjern unødvendig boolean i query-param til opprett stilling-modal

--- src/hurtiglenker/Hurtiglenker.tsx
+++ src/hurtiglenker/Hurtiglenker.tsx
@@ -19,7 +19,7 @@ const Hurtiglenker: FunctionComponent = () => {
             />
             <LenkepanelMedIkon
                 erSammeApp
-                href="/stillinger/minestillinger?visOpprettStillingModal=true"
+                href="/stillinger/minestillinger?visOpprettStillingModal"
                 tittel="Opprett ny stilling"
                 ikonSrc={blyant}
             />
