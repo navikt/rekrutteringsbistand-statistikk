6203983ea0e992eac3383ff348307401150a0092
Joar Aurdal <joar.aurdal@nav.no>
2022-12-15 10:52:49 +0100

    Fiks imports

--- src/statistikk/TiltaksstatistikkKategori.tsx
+++ src/statistikk/TiltaksstatistikkKategori.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from 'react';
 import css from './Tiltaksstatistikk.module.css';
-import classNames from 'classnames';
 import { Label, Detail } from '@navikt/ds-react';
 import { formaterSomProsentAvTotalen } from './Forespørsler';
 
