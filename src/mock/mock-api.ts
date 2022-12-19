b358d2e5105e63620771af42b54d601cc6bb1f15
Joar Aurdal <joar.aurdal@nav.no>
2022-12-19 13:02:21 +0100

    Fiks prettier feil

--- src/mock/mock-api.ts
+++ src/mock/mock-api.ts
@@ -9,7 +9,7 @@ fetchMock.get(`begin:${statistikkApiUrl}`, (url) => {
     const searchQuery = url.split('?').pop();
     const navKontor = new URLSearchParams(searchQuery).get('navKontor');
     const fraOgMed = new URLSearchParams(searchQuery).get('fraOgMed');
-    if (fraOgMed != null && +fraOgMed.substring(5, 7) % 2 == 1) {
+    if (fraOgMed != null && +fraOgMed.substring(5, 7) % 2 === 1) {
         return hentAntallFormidlinger('0000');
     }
 
@@ -21,7 +21,7 @@ fetchMock.get(`begin:${forespørslerApiUrl}`, (url): Svarstatistikk => {
     const navKontor = new URLSearchParams(searchQuery).get('navKontor');
 
     const fraOgMed = new URLSearchParams(searchQuery).get('fraOgMed');
-    if (fraOgMed != null && +fraOgMed.substring(5, 7) % 2 == 1) {
+    if (fraOgMed != null && +fraOgMed.substring(5, 7) % 2 === 1) {
         return hentForespørslerstatistikk('0000');
     }
 
