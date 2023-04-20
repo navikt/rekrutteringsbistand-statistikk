38dc81e5a10b1dd5530d0ad54216d370b32b1d0c
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2023-04-20 08:47:54 +0200

    Bruk Vite i stedet for Create React App (#45)
    * Deploy til dev
    
    * Migrer fra CRA til Vite
    
    * Fjern Jest fordi vi ikke har noen tester
    
    * Fjern noen setup-filer vi ikke trenger lenger
    
    * Hent riktige ressurser i server

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,17 +4,18 @@ import compression from 'compression';
 
 const port = process.env.PORT || 3000;
 const basePath = '/rekrutteringsbistand-statistikk';
-const buildPath = path.join(__dirname, '../build');
+const buildPath = path.join(__dirname, '../dist');
 
 const app = express();
 
 const startServer = () => {
     app.use(compression());
+
     app.get([`${basePath}/internal/isAlive`, `${basePath}/internal/isReady`], (_, res) =>
         res.sendStatus(200)
     );
 
-    app.use(`${basePath}/static`, express.static(buildPath + '/static'));
+    app.use(`${basePath}/assets`, express.static(buildPath + '/assets'));
     app.use(`${basePath}/asset-manifest.json`, express.static(`${buildPath}/asset-manifest.json`));
 
     app.listen(port, () => {
