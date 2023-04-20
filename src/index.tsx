852a4c2f37baf97484f5e4e583147c717ffdf1cb
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-04-20 08:24:55 +0200

    Migrer fra CRA til Vite

--- src/index.tsx
+++ src/index.tsx
@@ -4,26 +4,29 @@ import Navspa from '@navikt/navspa';
 import App, { AppProps } from './App';
 import Utviklingsapp from './utviklingsapp/Utviklingsapp';
 import { createRoot } from 'react-dom/client';
-import '@navikt/ds-css';
-import './index.css';
 import { Router } from 'react-router-dom';
+import './index.css';
 
-const skalEksporteres = process.env.REACT_APP_EXPORT || process.env.NODE_ENV === 'production';
-
-if (process.env.REACT_APP_MOCK) {
-    require('./mock/mock-api');
-}
-
-const AppMedCssScope: FunctionComponent<AppProps> = (props: AppProps) => (
+const AppMedRouter: FunctionComponent<AppProps> = (props: AppProps) => (
     <Router navigator={props.history} location={props.history.location}>
         <App {...props} />
     </Router>
 );
 
-if (skalEksporteres) {
-    Navspa.eksporter('rekrutteringsbistand-statistikk', AppMedCssScope);
-} else {
+const renderUtviklingsapp = async () => {
+    if (import.meta.env.VITE_MOCK === true) {
+        await import('./mock/mock-api');
+    }
+
     const container = document.getElementById('utviklingsapp');
     const root = createRoot(container!);
     root.render(<Utviklingsapp />);
+};
+
+const skalEksporteres = import.meta.env.VITE_EXPORT || import.meta.env.PROD;
+
+if (skalEksporteres) {
+    Navspa.eksporter('rekrutteringsbistand-statistikk', AppMedRouter);
+} else {
+    renderUtviklingsapp();
 }
